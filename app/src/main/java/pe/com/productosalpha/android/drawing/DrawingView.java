054ddf7fbbef13503e7363feb5ac03cf50028d40
Enrique Juan de Dios <juandedioz@gmail.com>
2016-08-07 13:41:15 -0500

    Add Layout

---
+++ app/src/main/java/pe/com/productosalpha/android/drawing/DrawingView.java
@@ -0,0 +1,21 @@
+package pe.com.productosalpha.android.drawing;
+
+import android.view.View;
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Created by user on 8/7/16.
+ */
+public class DrawingView extends View {
+    public DrawingView( Context context, AttributeSet attributeSet){
+        super(context,attributeSet);
+        setupDrawing();
+    }
+
+    private void setupDrawing(){
+        //get drawing area setup for interacction
+
+    }
+
+}
